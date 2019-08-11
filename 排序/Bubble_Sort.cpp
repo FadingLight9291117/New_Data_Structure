90e043e597296f56777fbb6d8e817949ffcc0316
L.Z.Che <FadingLight9291117@126.com>
2019-08-11 22:17:02 +0800

    添加了新排序

--- 排序/Bubble_Sort.cpp
+++ 排序/Bubble_Sort.cpp
@@ -1,7 +1,10 @@
+/***************/
+/*  冒泡排序    */
+/**************/
 #include<stdio.h>
 void Bubble_Sort(int *a,int n)
 {
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = n - 1; i >= 0; i--)    // 一趟冒泡
     {
         int flag = 0;
         for (int j = 0; j < i; j++)
@@ -11,10 +14,10 @@ void Bubble_Sort(int *a,int n)
                 int temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp;
-                flag = 1;
+                flag = 1;               // 发生了交换
             }
         }
-        if (flag == 0) 
+        if (flag == 0)                  // 全程无交换 
             break;        
     }
 }
