40c11916609980a3e9dc3a087274bf50b473e224
FadingLight <FadingLight9291117@126.com>
2020-08-04 10:31:30 +0800

    quicksort

---
+++ quick_sort.c
@@ -0,0 +1,134 @@
+/*
+    快速排序
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    两数交换函数
+*/
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/*
+    选主元
+    首尾和中间三个数中间大小的作为主元，并放在倒数第二个位置
+    返回主元
+*/
+int medium3(int *arr, int left, int right)
+{
+    int center = (left + right) / 2;
+    if (arr[left] > arr[center])
+    {
+        swap(arr + left, arr + center);
+    }
+    if (arr[center] > arr[right])
+    {
+        swap(arr + center, arr + right);
+    }
+    if (arr[left] < arr[center])
+    {
+        swap(arr + left, arr + center);
+    }
+    swap(arr + center, arr + right - 1);
+    return arr[right - 1];
+}
+
+/*
+    插入排序
+*/
+void insert_sort(int *arr, int left, int right)
+{
+    for (int i = left + 1; i <= right; i++)
+    {
+        int temp = arr[i];
+        int j;
+        for (j = i; i > left; j--)
+        {
+            if (arr[j - 1] > temp)
+            {
+                arr[j] = arr[j - 1];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j] = temp;
+    }
+}
+
+/*
+    快速排序核心函数
+*/
+void quickSort(int *arr, const int left, const int right)
+{
+    int cutOff = 5;                            //  定阈值
+    if (right - left >= cutOff)                // 元素个数大于阈值，使用快排
+    {                                          // 快速排序
+        int pivot = medium3(arr, left, right); // 选主元
+        int l = left - 1;
+        int r = right - 2;
+        while (1)
+        {
+            while (arr[l] < pivot) // 从左至右找到一个比主元大的元素
+            {
+                l++;
+            }
+            while (arr[r] > pivot) // 从右至左找到一个比主元小的元素
+            {
+                r--;
+            }
+            if (l < r) // 交换
+            {
+                swap(&arr[l], &arr[r]);
+            }
+            else
+            {
+                break;
+            }
+        }
+        swap(&arr[l], &arr[right - 1]);
+        quickSort(arr, left, l - 1);
+        quickSort(arr, l + 1, right);
+    }
+    else //  元素个数小于阈值，使用插入排序
+    {
+        insert_sort(arr, left, right);
+    }
+}
+
+/*
+    统一接口
+*/
+void quick_sort(int *arr, int len)
+{
+    quickSort(arr, 0, len - 1);
+}
+
+/*
+    测试
+*/
+int main()
+{
+    int n = 100;
+    int a[100];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand();
+    }
+    quick_sort(a, n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+        if (i > 0 && a[i] < a[i-1])
+        {
+            printf("error");
+            break;
+        }
+    }
+}
